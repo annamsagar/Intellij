2b6fd8c7c79172f1a19284292799c132bd02e228
annamsagar <annamsagar05@gmail.com>
2026-05-14 17:58:46 +0530

    refactor completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,18 +10,20 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Student s = new Student();
-        s.setRollNo(1);
-        s.setAge(46);
-        s.setSname("sagar");
-        Configuration cfg=new Configuration();
-        cfg.addAnnotatedClass(org.example.Student.class);
-
-        cfg.configure("hibernate.cfg.xml");
-        SessionFactory sf=cfg.buildSessionFactory();
+        s.setRollNo(3);
+        s.setAge(39);
+        s.setSname("sri ram");
+        SessionFactory sf=new Configuration()
+                .addAnnotatedClass(org.example.Student.class)
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+                //cfg.buildSessionFactory();
         Session s1=sf.openSession();
         Transaction trans=s1.beginTransaction();
         s1.persist(s);
         trans.commit();
+        s1.close();
+        sf.close();
         System.out.println(s);
     }
 }
